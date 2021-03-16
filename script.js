37dde7631188f28831a90f50207155668a00e3db
Miriam Kretschmann <miriam.kmann@gmail.com>
2021-03-16 13:51:13 +0100

    Module 4 V1

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+  var names = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
+  for (var i = 0; i < names.length; i++) {
+    if (names[i].toLowerCase().charAt() == "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
